b12572ba58da872de4d203668322444deca343e4
Lpi <thelpi.v3@gmail.com>
2025-03-10 23:26:31 +0100

    get confederation id

--- FmFileParse/Models/Country.cs
+++ FmFileParse/Models/Country.cs
@@ -5,6 +5,9 @@ namespace FmFileParse.Models;
 
 public class Country : BaseData
 {
+    [DataPosition(113)]
+    public short ConfederationId { get; set; }
+
     [DataPosition(4, Length = 50)]
     public string Name { get; set; } = string.Empty;
 
